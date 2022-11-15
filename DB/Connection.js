11d129733244c3f100d9ea208abcdb9f3884f77b
Lee Somi <101859625+IcebergSheep@users.noreply.github.com>
2022-11-16 01:16:40 +0900

    Update Connection.js

--- DB/Connection.js
+++ DB/Connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const URI = "mongodb+srv://Lavacat:soeun1206@cluster0.ya05snu.mongodb.net/?retryWrites=true&w=majority";
+const URI = "mongodb+srv://Lavacat:*********@cluster0.ya05snu.mongodb.net/?retryWrites=true&w=majority";
 
 const connectDB = async()=>{
     await mongoose.connect(URI);
